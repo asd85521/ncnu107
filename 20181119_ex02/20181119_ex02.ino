b9c2eccd25eb9e25f816608ad7b20bd4aabab64c
asd85521 <45555200+asd85521@users.noreply.github.com>
2018-12-03 17:13:41 +0800

    commit all

---
+++ 20181119_ex02/20181119_ex02.ino
@@ -0,0 +1,35 @@
+#define blueLed 13
+#define redLed 12
+
+void setup() {
+  // put your setup code here, to run once:
+     pinMode(blueLed,OUTPUT); //setmode for output
+     pinMode(redLed,OUTPUT); //setmode for output
+     digitalWrite(blueLed,LOW); // set pin to default value
+     digitalWrite(redLed,LOW); // set pin to default value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+     digitalWrite(blueLed,HIGH);
+     digitalWrite(redLed,HIGH);
+     delay(500);
+     digitalWrite(blueLed,LOW);
+     digitalWrite(redLed,LOW);
+     delay(500);
+     digitalWrite(blueLed,HIGH);
+     digitalWrite(redLed,HIGH);
+     delay(500);
+     digitalWrite(blueLed,LOW);
+     digitalWrite(redLed,LOW);
+     delay(500);
+     
+     digitalWrite(redLed,HIGH);
+     delay(1000);
+     digitalWrite(redLed,LOW);
+     delay(1000);
+     digitalWrite(blueLed,HIGH);
+     delay(1000);
+     digitalWrite(blueLed,LOW);
+     delay(1000);
+}
